9d8be6bb24518e479308b6599cd742d2fe4c8b41
mmenschig <marian.menschig@googlemail.com>
2017-12-24 13:08:11 -0800

    project update

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const routes = require("./app/routes");
-const mongoose = require("mongoose");
+const fs          = require("fs");
+const path        = require("path");
+const express     = require("express");
+const bodyParser  = require("body-parser");
+const mongoose    = require("mongoose");
+const morgan      = require("morgan");
+const routes      = require("./app/routes");
 
+
+// Creating Express App
 const app = express();
+
+// create a write stream (in append mode)
+let accessLogStream = fs.createWriteStream(
+  path.join(__dirname, './logs/access.log'),
+  {flags: 'a'}
+);
+
+app.use(morgan('combined', {stream: accessLogStream}))
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
+// Loading Routes
 app.use(routes);
 
 mongoose.connect('mongodb://localhost/juana', 
@@ -17,6 +30,6 @@ mongoose.connect('mongodb://localhost/juana',
 );
 mongoose.Promise = global.Promise;
 
-app.listen(3000, function() {
+app.listen(3001, function() {
   console.log("Server running on port 3000");
 })
