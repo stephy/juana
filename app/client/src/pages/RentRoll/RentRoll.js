0cb585d1542cd8ea3bebfd61b5d203088807f23a
mmenschig <marian.menschig@googlemail.com>
2018-01-10 08:21:03 -0800

    rentroll update

--- app/client/src/pages/RentRoll/RentRoll.js
+++ app/client/src/pages/RentRoll/RentRoll.js
@@ -21,16 +21,42 @@ class RentRoll extends Component {
       .catch(err => console.log(err))
   };
 
+  // TODO: requires major refactoring 
+  // Lots of components can be created here
   render() {
     return (
       <div className="section">
         <div className="container">
-          <h1 className="title">Rent roll for month of xxx</h1>
+          <h1 className="title is-size-1">Rent Roll</h1>
+          <h2 className="subtitle is-size-5">February 2018</h2>
           <div className="columns">
             <div className="column is-one-third">
               {this.state.units.map(unit => (
                 <div className="box">
-                  #{unit.unit}
+                  <article className="media">
+                      <div className="media-left">
+                        <figure className="image is-64x64 is-rounded">
+                          <img src="https://bulma.io/images/placeholders/128x128.png" alt="Image"/>
+                        </figure>
+                      </div>
+                  <div className="media-content">
+                    <div className="content">
+                      <p>
+                        <strong>#{unit.unit}</strong>
+                        <small> rent: ${unit.rent}</small>
+                        <br />
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+                      </p>
+                    </div>
+                    <nav className="level is-mobile">
+                      <div className="level-left">
+                        <a class="level-item">
+                          <span className="icon is-small"><i className="fas fa-edit"></i></span>
+                        </a>
+                      </div>
+                    </nav>
+                  </div>
+                  </article>
                 </div>
               ))}
             </div>
