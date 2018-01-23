7c7b6296f4790efffeb23c1f5d1ded08476b4568
mmenschig <marian.menschig@googlemail.com>
2018-01-22 22:13:13 -0800

    added auth route skeletons

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -1,9 +1,25 @@
 const router = require("express").Router();
 const passport = require("passport");
 
+
+// Matches GET '/auth/user'
 router.get("/user", function(req, res) {
   res.json({"test": "value"})
 })
 
+// Matches POST '/auth/login'
+router.post("/login", function(req, res) {
+  // do something
+})
+
+// Matches POST '/auth/register'
+router.post("/register", function(req, res) {
+  // do something
+})
+
+// Matches GET '/auth/logout'
+router.get("/logout", function(req, res) {
+  // do something
+})
 
 module.exports = router;
